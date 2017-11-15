83251011fcda6e2e26db7725237c04de4c475475
oscar santiago <santio1@unlv.nevada.edu>
2017-11-15 01:38:28 -0800

    table view grows dynamically, back-end still needs to be done.

--- OpenClass/OpenClass/Controllers/AnswersViewController.swift
+++ OpenClass/OpenClass/Controllers/AnswersViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class AnswersViewController: UIViewController {
+class AnswersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var commentTableView: UITableView!
+    @IBOutlet weak var commentTextBox: UITextField!
+    
+    @IBOutlet weak var commentAddButton: UIButton!
+    
+    let currentUser = "Oscar"
+    var recentComment = ""
+    var listOfComments = ["Yes","Alright, Thanks!"]
+    var listOfUsers=["Oscar","Oscar2"]
+    
+    @IBAction func commentAddButton(sender: UIButton)
+    {
+        recentComment = commentTextBox.text!
+        listOfComments.append(recentComment)
+        listOfUsers.append(currentUser)
+        self.commentTableView.reloadData()
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(listOfComments.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "commentCellPrototype", for: indexPath) as! AnswersViewControllerTableViewCell
+        cell.commentUserName.text = listOfUsers[indexPath.row]
+        cell.commentResponse.text = listOfComments[indexPath.row]
+       
+        return(cell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
