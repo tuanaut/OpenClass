7201d61fc6603a9f0b3c54470d9a35aa758605ba
Oscar Santiago <santio1@unlv.nevada.edu>
2017-11-28 01:10:36 -0800

    Answers are now writable/readable from database.

--- OpenClass/OpenClass/Controllers/AnswersViewController.swift
+++ OpenClass/OpenClass/Controllers/AnswersViewController.swift
@@ -7,31 +7,56 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseDatabase
+import Firebase
 
 class AnswersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var passedUserID : String!
+    var passedQuestion: String!
+    var passedFirstName : String!
+    var passedLastName : String!
+    var passedAnswerID: String!
+    var answerArray = [Comment] ()
+    var databaseRef = Database.database().reference()
+    
     
     @IBOutlet var commentTableView: UITableView!
     @IBOutlet weak var commentTextBox: UITextField!
     @IBOutlet weak var commentAddButton: UIButton!
     
-    // Fake info for testing, actual data will be from server.
-    var passedUserID : String!
-    var passedQuestion: String!
-    var passedFirstName : String!
-    var PassedLastName : String!
-    
-    let currentUser = "Oscar"
-    var recentComment = ""
-    var listOfComments = ["Yes","Alright, Thanks!"]
-    var listOfUsers=["Oscar","User"]
+   
+
     
     // Button to add comment to Question.
     @IBAction func commentAddButton(sender: UIButton)
     {
-        recentComment = commentTextBox.text!
-        listOfComments.append(recentComment)
-        listOfUsers.append(currentUser)
-        self.commentTableView.reloadData()
+        if (commentTextBox.text?.isEmpty)!
+        {
+            return
+        }else
+        {
+            //Fetch Data
+            let values = ["Commenter": passedFirstName, "Comment": commentTextBox.text, "id": passedAnswerID]
+            databaseRef.child("comments").childByAutoId().setValue(values,withCompletionBlock: {(error,ref) in
+                if (error == nil)
+                {
+                    let answer = Comment(commentPosted: self.commentTextBox.text!, username: self.passedFirstName, notesID: self.passedAnswerID)
+                    self.answerArray.append(answer)
+                    self.commentTableView.reloadData()
+                    
+                    let indexPath = IndexPath(row: self.answerArray.count - 1, section: 0)
+                    self.commentTableView.scrollToRow(at: indexPath, at: UITableViewScrollPosition.middle, animated: true)
+                    self.commentTextBox.text?.removeAll()
+                }else
+                {
+//                    self.displayMyAlertMessage(userMessage: (error?.localizedDescription)!)
+                }
+                
+            })
+            
+        }
         
     }
     
@@ -44,15 +69,15 @@ class AnswersViewController: UIViewController, UITableViewDelegate, UITableViewD
     // Set number of rows to load into table if not one.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return(listOfComments.count)
+        return(answerArray.count)
     }
     
     // Load table cells with information.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "commentCellPrototype", for: indexPath) as! AnswersViewControllerTableViewCell
-        cell.commentUserName.text = listOfUsers[indexPath.row]
-        cell.commentResponse.text = listOfComments[indexPath.row]
+        cell.commentUserName.text = answerArray[indexPath.row].commentPosted
+        cell.commentResponse.text = passedFirstName
        
         return(cell)
     }
@@ -61,9 +86,41 @@ class AnswersViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Hiddes Keyboard when tapped in the view
+        self.hideKeyboardWhenTappedAround()
+        // Dynamically increase amount of space in cell
+        commentTableView.rowHeight = UITableViewAutomaticDimension
+        // Do not show empty cells
+        commentTableView.tableFooterView = UIView(frame: CGRect.zero)
+    
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        answerArray.removeAll()
+        commentTableView.reloadData()
+        fetchAnswers()
+        commentTableView.reloadData()
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    private func fetchAnswers()
+    {
+        let ref = databaseRef.child("comments").queryOrdered(byChild: "id").queryEqual(toValue: passedAnswerID)
+        ref.observeSingleEvent(of: .value, with: { (snapshot)
+            in
+            for childSnapshot in snapshot.children {
+                let newAnswer = Comment(snapshot: childSnapshot as! DataSnapshot)
+                self.answerArray.append(newAnswer)
+            }
+            self.commentTableView.reloadData()
+            
+        })
+    }
+    
     
     override func didReceiveMemoryWarning()
     {
